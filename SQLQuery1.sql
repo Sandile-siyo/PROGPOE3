7c7d8365ba47cc11a55bdbcfdb14e21de8d4a64c
Sandile-siyo <st10476405@rcconnect.edu.za>
2026-06-26 15:06:16 +0200

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,25 @@
+﻿--creating a database called tasks_prog--
+ create database tasks_prog;
+
+ --use the tasks_prog database--
+ use tasks_prog;
+
+ --creating a table [ entity ]
+ --columns are called task_id, task_name, task_description, task_status--
+ --List of the columns
+ --task_id datatype int, and auto-increment
+ --task_name datatype varchar(amount of letters)
+ --task_description datatype varchar(amount of letters)
+ --task_dueDate datatype varchar(50)
+ --task status datatype varchar 
+ --create the table as tasks when doing the POE
+ create table poe_tasks(
+   task_id int primary key identity(1,1) ,
+   task_name varchar(100) ,
+   task_description varchar(100) ,
+   task_dueDate varchar(20) ,
+   task_status varchar(20)
+ );
+
+ --select all from the table poe_tasks--
+ select * from poe_tasks;
